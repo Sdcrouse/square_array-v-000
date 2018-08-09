3d4f30f7ea0d8727881c330b692f324a9ff5e7a5
Steven Crouse <sdcrouse91@gmail.com>
2018-08-09 00:07:33 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -4,8 +4,8 @@ def square_array(array)
   squares
   
   # Note: if I want to do this with strings AND numbers, here's how:
-  # squares = []
-  # array.each{|item| squares.push(item * item)} # ** is not defined for strings!
-  # squares
+  squares = []
+  array.each{|item| squares.push(item * item)} # ** is not defined for strings!
+  squares
   # If item == "word", then item * item returns "wordword".
 end
