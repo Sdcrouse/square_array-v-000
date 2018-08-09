5f52dd9bef9aa353f869fdf7ab63ef1763f8edfb
Steven Crouse <sdcrouse91@gmail.com>
2018-08-09 00:06:01 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,4 +2,9 @@ def square_array(array)
   squares = []
   array.each {|number| squares.push(number ** 2)}
   squares
+  
+  # Note: if I want to do this with strings AND numbers, here's how:
+  # squares = []
+  # array.each{|item| squares.push(item * item)} # ** is not defined for strings!
+  # squares
 end
